3d4e199702f91552b5a220b4f6f8ccac75694582
Brayan Texis <utp0145349@alumno.utpuebla.edu.mx>
2023-09-29 17:49:10 -0600

    Creación de proyecto React con OpenStreetMap

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MapNavigation from "./components/MapNavigation"
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <MapNavigation/>
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/components/MapNavigation.jsx
@@ -0,0 +1,34 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const MapNavigation = () => {
+
+    const PositionPuebla = [19.0431, -98.1980]; //latitud y longitud del Estado de Puebla
+
+    const markers = [
+        { position: [19.0583869, -98.1545258], name: 'Universidad Tecnológica de Puebla' },
+        { position: [19.0428904, -98.200904], name: 'Catedral de la Cd. de Puebla' },
+        { position: [19.0966071, -98.2441535], name: 'Fuente de los Frailes' },
+        { position: [19.05108, -98.17518], name: 'Fuerte de Loreto' },
+    ];
+
+  return (
+    <div className=".map-container">
+    <MapContainer center={PositionPuebla} zoom={13} style={{ height: '100vh', width: '100%' }} >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+        {markers.map((marker, index) => (
+          <Marker key={index} position={marker.position}>
+            <Popup>
+              {marker.name}
+            </Popup>
+          </Marker>
+        ))}
+    </MapContainer>
+  </div>
+  );
+};
+
+export default MapNavigation;
