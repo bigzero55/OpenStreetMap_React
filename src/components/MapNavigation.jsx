cca20f7fb1a51c2c09246bf78ecab2734ff53f8d
Abdul Rojak <abdulrozaaq5@outlook.co.id>
2023-10-24 07:11:05 +0700

    tambah lokasi

--- src/components/MapNavigation.jsx
+++ src/components/MapNavigation.jsx
@@ -1,35 +1,69 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { useState } from "react";
+
+function LocationMarker() {
+
+  const [position, setPosition] = useState(null)
+  const map = useMapEvents({
+    click() {
+      map.locate()
+    },
+    locationfound(e) {
+      setPosition(e.latlng)
+      map.flyTo(e.latlng, map.getZoom())
+    },
+  })
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  )
+}
 
 const MapNavigation = () => {
+  const PositionPuebla = [-6.968326823991348, 107.62729336882325]; // Garis lintang dan bujur Negara Bagian Puebla
 
-    const PositionPuebla = [-6.968326823991348, 107.62729336882325]; // Garis lintang dan bujur Negara Bagian Puebla
+  const markers = [
+    { position: [-6.967708914879933, 107.62773543893485], name: "Real Kost" },
+    {
+      position: [-6.968649211031864, 107.62717608491147],
+      name: "Pondok Izzati",
+    },
+    {
+      position: [-6.968550703904539, 107.62852936076335],
+      name: "OYO Socialite",
+    },
+    {
+      position: [-6.968613390259261, 107.6263641193572],
+      name: "warung pak agus",
+    },
+    { position: [-6.9672253332680665, 107.62705880096115], name: "Gelewo" },
+  ];
 
-    const markers = [
-        { position: [-6.967708914879933, 107.62773543893485], name: 'Real Kost' },
-        { position: [-6.968649211031864, 107.62717608491147], name: 'Pondok Izzati' },
-        { position: [-6.968550703904539, 107.62852936076335], name: 'OYO Socialite' },
-        { position: [-6.968613390259261, 107.6263641193572], name: 'warung pak agus' },
-        { position: [-6.9672253332680665, 107.62705880096115], name: 'Gelewo' }
-    ];
-    
+
 
   return (
     <div className=".map-container">
-    <MapContainer center={PositionPuebla} zoom={16} style={{ height: '92vh', width: '100%' }} >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> kontributor'
+      <MapContainer
+        center={PositionPuebla}
+        zoom={16}
+        style={{ height: "92vh", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> kontributor'
         />
-        {markers.map((marker, index) => (
+        {/* {markers.map((marker, index) => (
           <Marker key={index} position={marker.position}>
-            <Popup>
-              {marker.name}
-            </Popup>
+            <Popup>{marker.name}</Popup>
           </Marker>
-        ))}
-    </MapContainer>
-  </div>
+        ))} */}
+
+        <LocationMarker />
+      </MapContainer>
+    </div>
   );
 };
 
